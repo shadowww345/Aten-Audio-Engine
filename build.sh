b24d4a909fc18f9677c607982a67f5579c97b426
shadow <acaromer330@gmail.com>
2026-09-04 12:16:51 +0300

    Modify build.sh to create shared library libatenaudio.so
    Changed build script to create a shared library and update library cache.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!bin/bash
 
-gcc main.c eng/eng_pipewire.c eng/wav.c eng/effects/reverb/reverb.c -o aten $(pkg-config --cflags --libs libpipewire-0.3) -lm -Ieng
+gcc -fPIC -shared aten.c eng/eng_pipewire.c eng/wav.c eng/mp3.c eng/effects/reverb/reverb.c -o libatenaudio.so -lpthread $(pkg-config --cflags --libs libpipewire-0.3) -lm -Ieng
+sudo cp libatenaudio.so /usr/local/lib/
+sudo ldconfig
